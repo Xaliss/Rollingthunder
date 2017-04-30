8e218b84d8e35a9c1e989f360399096b8dc0374c
MikkaH Savva <diopsys0hotmail.com>
2017-04-30 12:12:00 +0200

    Commited H3

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-	return ('<h1>Deploye ds Heroku</h1>')
+	return '<h1>Deploye ds Heroku</h1><h3>On y Arrivera !!!</h3>'
