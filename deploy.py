84530c04fdb94230c56405d12d789e231a6e8479
MikkaH Savva <diopsys0hotmail.com>
2017-05-01 11:55:34 +0200

    Commit F

--- deploy.py
+++ deploy.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET"])
 def index():
-	return render_template('base1.html')
+	return render_template('mapage.html')
 	#return '<H1>Et Voila !!!</H1>'
 
 if __name__ == "__main__":
